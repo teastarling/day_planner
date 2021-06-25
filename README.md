cf7a108546ca01b586f3beb40ea0b6337ea06af2
Tyler Starling <tyler.a.starling@gmail.com>
2021-06-25 17:28:09 -0400

    transferred functional for-loop to change hour colors to each in jquery and completed the save and load of data to and from local storage in jquery as well. added changes to html in order to accomodate jquery functionality and tweaked css in order to achieve correct appearance of time blocks. created readme with included screen shot and important code jquery code launched on page load

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# day_planner
+# Weekly Day Planner
+
+Utilizing JQuery, Moment.js, and Bootstrap, this site shows users the current day and blocks encompassing usual office hours (9A-5P) which change color depending on the current hour on page load. Tasks can be written into the input field for each hour and saved to local storage in order to retain tasks even if the page is exited and later returned to.
+
+## Screen Shot
+
+Site appearance on load [here](./assets/images/dayschedule.png).
+
+
+## Usage
+
+```jquery
+$('.input-group').each(function() {
+    var id = $(this).children(".form-control").attr("id");
+    var task = localStorage.getItem(id);
+
+    $(this).children(".form-control").val(task);
+
+    if (id > currentHour) {           
+            $(this).children(".form-control").addClass("future");
+    } else if (id < currentHour) {
+            $(this).children(".form-control").addClass("past");
+    } else {
+            $(this).children(".form-control").addClass("present");
+    }
+    });
+```
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
+// variables to declare time (current) for functions below
 var today = moment();
-var []
+var currentHour = today.hours();
+//  moment.js jquery command to run the current day in the format of the day of the week, followed by the month and the date
+$('#currentDay').text(today.format("dddd[,] MMM Do"));
+// function to save the information placed in the button sibling input field when the save buttons are clicked; information is saved with the id of the input element used as the local storage key
+$(".btn").click(function(){
+    var id = $(this).siblings(".form-control").attr("id");
+    var task = $(this).siblings(".form-control").val();
+    localStorage.setItem(id, task);
+})
+// when page loads, each input group runs through local storage and assigns any local storage items with a key that matches the child input element's id to the input field. After that, an if/else statement to color the blocks is run, still using the parent "input-group" class object as the main header and the child input element to compare the id to the global variable "currentHour" that is able to find the current hour using moment.js methods, and then draw the css color parameters from the css documents by adding the class do that element based on the boolean of the if/else statements
+$('.input-group').each(function() {
+    var id = $(this).children(".form-control").attr("id");
+    var task = localStorage.getItem(id);
 
-$('#currentDay').text(today.format("dddd[,] MMM Do"))
+    $(this).children(".form-control").val(task);
 
-function colorChange() {
-    var timeNow = moment().format("k");
-
-    for (var i=9; i < 18; i++) {
-        if (timeNow == i) {           
-            document.querySelector('[data-time="'+ i +'"]').style.backgroundColor = "#ff6961";
-        } else if (timeNow > i) {
-            document.querySelector('[data-time="'+ i +'"]').style.backgroundColor = "#d3d3d3";
-        } else {
-            document.querySelector('[data-time="'+ i +'"]').style.backgroundColor = "#77dd77";
-        }
+    if (id > currentHour) {           
+            $(this).children(".form-control").addClass("future");
+    } else if (id < currentHour) {
+            $(this).children(".form-control").addClass("past");
+    } else {
+            $(this).children(".form-control").addClass("present");
     }
-}
-
-
-colorChange();
+    });
